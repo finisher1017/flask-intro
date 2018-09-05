e56eed0e5ef2cb5743906642f260c95fdf37a491
finisher1017 <finisher1017@gmail.com>
2018-09-05 15:48:15 -0700

    bcrypt problem

--- db_create_users.py
+++ db_create_users.py
@@ -2,8 +2,8 @@ from project import db
 from project.models import User
 
 # insert data
-db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
-db.session.add(User("jonny", "jonny@seubs.com", "jonnypass"))
+#db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
+db.session.add(User("jonathan", "jonathan@seubs.com", "jonathan"))
 
 # commit the changes
 db.session.commit()
